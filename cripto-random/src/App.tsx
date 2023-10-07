cfa02ca173f7490f1e01f1167bedde1d5931de4c
d3us <dnarino@gmail.com>
2023-10-06 20:53:53 -0500

    crypthographic app done without ReactQuery

--- cripto-random/src/App.tsx
+++ cripto-random/src/App.tsx
@@ -1,35 +1,119 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useReducer, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Button, Grid, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import { ElectricBolt } from "@mui/icons-material";
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+  padding: "2rem 1rem",
+}));
+
+const getRandomNumberFromAPI = async (): Promise<number> => {
+  const res = await fetch(
+    "https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new"
+  );
+  // throw new Error("Something is Wrong ...");
+  const numberString = await res.text();
+  return +numberString;
+};
+
+export const App = () => {
+  const [number, setNumber] = useState<number>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isError, setIsError] = useState<string>();
+  const [key, forceRefetch] = useReducer((x) => x + 1, 0);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getRandomNumberFromAPI()
+      .then((promiseNumber) => setNumber(promiseNumber))
+      .catch((error) => setIsError(error.message));
+  }, [key]);
+
+  useEffect(() => {
+    if (number) setIsLoading(false);
+  }, [number]);
+
+  useEffect(() => {
+    if (isError) setIsLoading(false);
+  }, [isError]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Grid
+        container
+        spacing={2}
+        direction={"column"}
+        wrap="nowrap"
+        justifyContent={"space-evenly"}
+        alignItems={"center"}
+        gap={5}
+        sx={{
+          // border: "solid",
+          padding: "20% ",
+        }}
+      >
+        <Grid
+          item
+          xs={12}
+          sx={{
+            // border: "solid blue",
+            textAlign: "center",
+          }}
+        >
+          <Typography variant="h3">Cryptographic Numbers App</Typography>
+        </Grid>
 
-export default App
+        <Grid
+          item
+          flexGrow={3}
+          xs={12}
+          sx={
+            {
+              // border: "solid green",
+            }
+          }
+        >
+          <Item>
+            {isLoading ? (
+              <Typography>Loading...</Typography>
+            ) : (
+              !isError && (
+                <Typography
+                  component="p"
+                  variant="h5"
+                >
+                  Crypto Random Number: {number}
+                </Typography>
+              )
+            )}
+            {isError && <h2>{isError}</h2>}
+          </Item>
+        </Grid>
+
+        <Grid
+          item
+          sx={
+            {
+              // border: "solid red",
+            }
+          }
+        >
+          <Button
+            onClick={forceRefetch}
+            disabled={isLoading}
+            variant="contained"
+            endIcon={<ElectricBolt/>}
+            size={"large"}
+          >
+            New 
+          </Button>
+        </Grid>
+      </Grid>
+    </>
+  );
+};

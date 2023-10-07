ca83ef4a605c11b784b48f0c880a4e9334e00fba
d3us <dnarino@gmail.com>
2023-10-07 18:06:28 -0500

    install axios library create http get request to api and set interface ts

--- 02-react-query-issues/src/issues/components/LabelPicker.tsx
+++ 02-react-query-issues/src/issues/components/LabelPicker.tsx
@@ -1,14 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import { githubAPI } from "../../API/githubAPI";
+import { Label } from "../interfaces/label";
+
+const getLabelsAPI = async (): Promise<Label[]> => {
+  const { data } = await githubAPI.get<Label[]>("/labels");
+  return data;
+};
 
 export const LabelPicker = () => {
+  const labelsQuery = useQuery(["labels"], getLabelsAPI, {
+    refetchOnWindowFocus: true,
+  });
+
   return (
-    <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-        >
-            Primary
-        </span>
-        
-    </div>
-  )
-}
+    <>
+      <span
+        className="badge rounded-pill m-1 label-picker"
+        style={{ border: `1px solid #ffccd3`, color: "#ffccd3" }}
+      >
+        Primary
+      </span>
+    </>
+  );
+};
--- 02-react-query-issues/src/main.tsx
+++ 02-react-query-issues/src/main.tsx
@@ -5,6 +5,7 @@ import { RouterProvider } from 'react-router';
 import { router } from './router';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 //! Remover enable css source maps
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -15,6 +16,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
       <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>

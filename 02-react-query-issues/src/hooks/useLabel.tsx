3b121bd90f45e5ff55db53c8eb24529c25eb589c
d3us <dnarino@gmail.com>
2023-10-07 19:07:56 -0500

    create sleep helper and set labels http request to fresh for 1 hour

--- 02-react-query-issues/src/hooks/useLabel.tsx
+++ 02-react-query-issues/src/hooks/useLabel.tsx
@@ -2,13 +2,17 @@ import { useQuery } from "@tanstack/react-query";
 
 import { githubAPI } from "../API/githubAPI";
 import { Label } from "../issues/interfaces/label";
+import { sleep } from "../helpers/sleep";
 
 const getLabelsAPI = async (): Promise<Label[]> => {
+  await sleep(5);
   const { data } = await githubAPI.get<Label[]>("/labels");
   return data;
 };
 
 export const useLabel = () => {
-  const labelsQuery = useQuery(["labels"], getLabelsAPI);
+  const labelsQuery = useQuery(["labels"], getLabelsAPI, {
+    staleTime: 1000 * 60 * 60,
+  });
   return labelsQuery;
 };
--- 02-react-query-issues/src/issues/components/LabelPicker.tsx
+++ 02-react-query-issues/src/issues/components/LabelPicker.tsx
@@ -1,16 +1,22 @@
 import { useLabel } from "../../hooks/useLabel";
 
-
 export const LabelPicker = () => {
   const labels = useLabel();
+  if (labels.isLoading) return <h1>Is loading....</h1>;
   return (
     <>
-      <span
-        className="badge rounded-pill m-1 label-picker"
-        style={{ border: `1px solid #ffccd3`, color: "#ffccd3" }}
-      >
-        Primary
-      </span>
+      {labels.data?.map((label) => (
+        <span
+          key={label.id}
+          className="badge rounded-pill m-1 label-picker"
+          style={{
+            border: `1px solid #${label.color}`,
+            color: `#${label.color}`,
+          }}
+        >
+          {label.name}
+        </span>
+      ))}
     </>
   );
 };

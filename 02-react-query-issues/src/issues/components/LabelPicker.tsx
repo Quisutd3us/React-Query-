d9f4e67ced7de59fed899cd62c01df3265b08219
d3us <dnarino@gmail.com>
2023-10-14 17:25:50 -0500

    function selected labels done

--- 02-react-query-issues/src/issues/components/LabelPicker.tsx
+++ 02-react-query-issues/src/issues/components/LabelPicker.tsx
@@ -1,8 +1,14 @@
+import { FC } from "react";
 import { useLabel } from "../../hooks/useLabel";
 
 import { LoadingIcon } from "../../shared/components/LoadingIcon";
 
-export const LabelPicker = () => {
+interface Props {
+  selectedLabels: string[];
+  onChange: (labelName: string) => void;
+}
+
+export const LabelPicker:FC<Props> = ({onChange, selectedLabels}) => {
   const labels = useLabel();
   if (labels.isLoading) return <LoadingIcon/>;
   return (
@@ -10,11 +16,14 @@ export const LabelPicker = () => {
       {labels.data?.map((label) => (
         <span
           key={label.id}
-          className="badge rounded-pill m-1 label-picker"
+          className={`badge rounded-pill m-1 label-picker ${
+            selectedLabels.includes(label.name) ? "active-label" : ""
+          }`}
           style={{
             border: `1px solid #${label.color}`,
             color: `#${label.color}`,
           }}
+          onClick={() => onChange(label.name)}
         >
           {label.name}
         </span>

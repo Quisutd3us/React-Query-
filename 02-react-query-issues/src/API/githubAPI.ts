f4b84cf992ed12244980c7dd95390c66a94f3dce
d3us <dnarino@gmail.com>
2023-10-17 14:24:34 -0500

    del File

--- 02-react-query-issues/src/API/githubAPI.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-
-export const githubAPI = axios.create({
-    baseURL:'https://api.github.com/repos/facebook/react',
-    headers: {
-        Authorization:'Bearer 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'
-    }
-});

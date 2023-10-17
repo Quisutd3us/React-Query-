cdb2bf9940e5bdc73b8a600e2357e7c91ea88b6a
d3us <dnarino@gmail.com>
2023-10-17 13:49:45 -0500

    create usery function to retrieve comments and render

--- 02-react-query-issues/src/API/githubAPI.ts
+++ 02-react-query-issues/src/API/githubAPI.ts
@@ -4,6 +4,6 @@ import axios from "axios";
 export const githubAPI = axios.create({
     baseURL:'https://api.github.com/repos/facebook/react',
     headers: {
-        Authorization:'Bearer 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'
+        Authorization:'Bearer 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'
     }
 });
--- 02-react-query-issues/src/hooks/useIssue.tsx
+++ 02-react-query-issues/src/hooks/useIssue.tsx
@@ -1,19 +1,27 @@
 import { useQuery } from "@tanstack/react-query";
 
-import { githubAPI } from "../API/githubAPI";
+import { githubAPI } from '../API/githubAPI';
 import { Issue } from "../issues/interfaces";
+import { sleep } from "../helpers/sleep";
 
 const getissueAPI = async (issueId: number): Promise<Issue> => {
+  await sleep(2);
   const { data } = await githubAPI.get<Issue>(`/issues/${issueId}`);
- 
   return data;
 };
 
+const getcommentsAPI = async(issueId: number):Promise<Issue[]>=>{
+  await sleep(2);
+  const {data} = await githubAPI.get<Issue []>(`/issues/${issueId}/comments`);
+  return data
+};
 
 export const useIssue =(issueNumber:number)=>{
-    const issueQuery =useQuery(
-        ["issue",issueNumber],
-        ()=>getissueAPI(issueNumber)
-    )
-    return { issueQuery };
+    const issueQuery = useQuery(["issue", issueNumber], () =>
+      getissueAPI(issueNumber)
+    );
+    const commentsQuery = useQuery(["issues", issueNumber, "comments"], () =>
+      getcommentsAPI(issueNumber)
+    );
+    return { issueQuery, commentsQuery };
 }

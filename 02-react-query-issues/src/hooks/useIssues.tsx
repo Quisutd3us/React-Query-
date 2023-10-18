e54a6a8a5f7376421555512c233f4ce55fb06d50
d3us <dnarino@gmail.com>
2023-10-18 12:14:13 -0500

    update render by selectedLabels using cache with reactQuery

--- 02-react-query-issues/src/hooks/useIssues.tsx
+++ 02-react-query-issues/src/hooks/useIssues.tsx
@@ -5,22 +5,27 @@ import { githubAPI } from "../API/githubAPI";
 import { Issue, State } from "../issues/interfaces";
 import { sleep } from "../helpers/sleep";
 
-interface Props{
-  labels:string[];
-  state?:State
+interface Props {
+  labels: string[];
+  state?: State;
 }
 
-const getIssuesAPI = async (labels:string[]=[],state?:State): Promise<Issue[]> => {
-
+const getIssuesAPI = async (
+  labels: string[] = [],
+  state?: State
+): Promise<Issue[]> => {
   const params = new URLSearchParams();
-  if(state) params.append('state',state);
-  if(labels.length>0) params.append('labels',labels)
+  if (state) params.append("state", state);
+  if (labels.length > 0) {
+    const labelsString = labels.join(",");
+    params.append("labels", labelsString);
+  }
   sleep(2);
-  const { data } = await githubAPI.get<Issue[]>("/issues",{params});
+  const { data } = await githubAPI.get<Issue[]>("/issues", { params });
   return data;
 };
 
-export const useIssues = ({labels,state}:Props) => {
+export const useIssues = ({ labels, state }: Props) => {
   const issuesQuery = useQuery(["issues", { labels, state }], () =>
     getIssuesAPI(labels, state)
   );
